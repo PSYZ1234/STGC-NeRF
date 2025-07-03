9325dc2f5aefc118bf8e76e0f3db607d74f9ec2b
shangshu.yu <psyz1666@gmail.com>
2025-07-03 11:30:04 +0800

    Add files via upload

---
+++ run_ours.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+python main_ours.py \
+--config configs/kitti360_1538.txt \
+--workspace log/kitti360_1538 \
+
+python main_ours.py \
+--config configs/kitti360_1728.txt \
+--workspace log/kitti360_1728 \
+
+python main_ours.py \
+--config configs/kitti360_1908.txt \
+--workspace log/kitti360_1908 \
+
+python main_ours.py \
+--config configs/kitti360_2350.txt \
+--workspace log/kitti360_2350 \
+
+python main_ours.py \
+--config configs/kitti360_3353.txt \
+--workspace log/kitti360_3353 \
+
+python main_ours.py \
+--config configs/kitti360_4950.txt \
+--workspace log/kitti360_4950 \
+
+python main_ours.py \
+--config configs/kitti360_8120.txt \
+--workspace log/kitti360_8120 \
+
+python main_ours.py \
+--config configs/kitti360_10200.txt \
+--workspace log/kitti360_10200 \
+
+python main_ours.py \
+--config configs/kitti360_10750.txt \
+--workspace log/kitti360_10750 \
+
+python main_ours.py \
+--config configs/kitti360_11400.txt \
+--workspace log/kitti360_11400 \
+
+python main_ours.py \
+--config configs/nuScenes_450.txt \
+--workspace log/nuScenes_450 \
+
+python main_ours.py \
+--config configs/nuScenes_1250.txt \
+--workspace log/nuScenes_1250 \
+
+python main_ours.py \
+--config configs/nuScenes_1600.txt \
+--workspace log/nuScenes_1600 \
+
+python main_ours.py \
+--config configs/nuScenes_2200.txt \
+--workspace log/nuScenes_2200 \
+
+python main_ours.py \
+--config configs/nuScenes_3180.txt \
+--workspace log/nuScenes_3180 \
